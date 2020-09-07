b2997a94efe8474cc45b814cab9bd45295be2f7d
ob1-cuna <anacletocuna@sapo.pt>
2020-09-07 14:56:02 +0200

    Adição de pacote para PDF.

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
 
         /*
@@ -228,7 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Carbon' => 'Carbon\Carbon',
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
