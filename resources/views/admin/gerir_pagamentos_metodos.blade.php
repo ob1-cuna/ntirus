e8d500c76abb5ca613742b9177634d1877640004
ob1-cuna <anacletocuna@sapo.pt>
2020-09-13 13:16:44 +0200

    actualização de formas de pagamentos

--- resources/views/admin/gerir_pagamentos_metodos.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.layouts.app')
-@section('title', 'Definições da Conta' )
-@section('descricao', 'Página de operações relativas â conta do usuário da Ntirus.' )
-@section('content')
-
-@endsection
--- resources/views/admin/gerir_trabalhos_list_admin.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.layouts.app')
-@section('title', 'Categorias' )
-@section('descricao', 'Página de gestão das categorias dos usuários da Aplicação.' )
-@section('content')
-    Gerir
-@endsection()
