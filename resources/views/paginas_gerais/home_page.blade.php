82c4897890bd2e255201696781e4e864becb5783
ob1-cuna <anacletocuna@sapo.pt>
2020-09-16 08:32:57 +0200

    adição de categorias e icones à pagina inicial

--- resources/views/paginas_gerais/home_page.blade.php
+++ resources/views/paginas_gerais/home_page.blade.php
@@ -18,74 +18,70 @@
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
                         <img src="{{ asset('images/categories/cabelo.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                        <a href="{{ route ('trabalhos.list', ['cat' => 'cabelereiro-estetica']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
                             <p class="card-text text-center text-capitalize">Cabeleireiro & Estética</p>
-                        </div>
+                        </a>
                         </div>
                     </div>
+
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
-                        <img src="{{ asset('images/categories/design-grafico.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
-                            <p class="card-text text-center text-capitalize">Design Grafico</p>
-                        </div>
+                            <img src="{{ asset('images/categories/carpentaria.svg') }}" class="card-img-top" alt="">
+                            <a href="{{ route ('trabalhos.list', ['cat' => 'carpetaria']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                                <p class="card-text text-center text-capitalize">Carpetaria</p>
+                            </a>
                         </div>
                     </div>
+
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
                         <img src="{{ asset('images/categories/electricidade.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                        <a href="{{ route ('trabalhos.list', ['cat' => 'electricidade']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
                             <p class="card-text text-center text-capitalize">Electricidade</p>
-                        </div>
+                        </a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
                         <img src="{{ asset('images/categories/instalacao-tv.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                        <a href="{{ route ('trabalhos.list', ['cat' => 'instalacao-tv']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
                             <p class="card-text text-center text-capitalize">Instalação de TV</p>
-                        </div>
+                        </a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
-                        <img src="{{ asset('images/categories/meu_file.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary  btn-block btn-lg btn-square border-0">
-                            <p class="card-text text-center text-capitalize">1Programação</p>
-                        </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
-                        <div class="card">
-                        <img src="{{ asset('images/categories/meu_file.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
-                            <p class="card-text text-center text-capitalize">Pintura</p>
-                        </div>
+                        <img src="{{ asset('images/categories/culinaria.svg') }}" class="card-img-top" alt="">
+                        <a href="{{ route ('trabalhos.list', ['cat' => 'culinaria']) }}" class="card-body btn btn-outline-secondary  btn-block btn-lg btn-square border-0">
+                            <p class="card-text text-center text-capitalize">Culinária</p>
+                        </a>
                         </div>
                     </div>
-
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
-                        <img src="{{ asset('images/categories/meu_file.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
-                            <p class="card-text text-center text-capitalize">Programação</p>
-                        </div>
+                            <img src="{{ asset('images/categories/code.svg') }}" class="card-img-top" alt="">
+                            <a href="{{ route ('trabalhos.list', ['cat' => 'dev-web']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                                <p class="card-text text-center text-capitalize">Desenvolvimento Web</p>
+                            </a>
                         </div>
                     </div>
-
                     <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
                         <div class="card">
-                        <img src="{{ asset('images/categories/meu_file.svg') }}" class="card-img-top" alt="">
-                        <div class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
-                            <p class="card-text text-center text-capitalize">Programação</p>
+                            <img src="{{ asset('images/categories/design-grafico.svg') }}" class="card-img-top" alt="">
+                                <a href="{{ route ('trabalhos.list', ['cat' => 'design-grafico']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                                    <p class="card-text text-center text-capitalize">Design Grafico</p>
+                                </a>
                         </div>
+                    </div>
+                    <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-10 mb-4">
+                        <div class="card">
+                        <img src="{{ asset('images/categories/edicao-de-video.svg') }}" class="card-img-top" alt="">
+                        <a href="{{ route ('trabalhos.list', ['cat' => 'edicao-de-video']) }}" class="card-body btn btn-outline-secondary btn-lg btn-square border-0">
+                            <p class="card-text text-center text-capitalize">Edição de Vídeo</p>
+                        </a>
                         </div>
                     </div>
             </div>
-        </div>
-        <div class="align-middle text-center">
-            <a class="btn btn-outline-secondary btn-lg" href="#" role="button">Ver Todas</a>
         </div>
 
         <h4>Como Funciona</h4>
@@ -94,30 +90,22 @@
                 <div class="col-lg-3 col-xl-3 col-md-3 col-sm-6 col-xs-6 text-center">
                     <object type="image/svg+xml" data="{{ asset('images/passos/passo_1.svg') }}" style="max-height: 150px"></object>
                     <h6><strong>Publique um Trabalho</strong></h6>
-                    <p>Lorem ipsum dolor sit amet,
-                        consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor.</p>
+                    <p>Começe publicando um trabalho para os freelancers possam concorrer.</p>
                 </div>
                 <div class="col-lg-3 col-xl-3 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <object type="image/svg+xml" data="{{ asset('images/brand_ntitus/logotype.svg') }}" style="max-height: 150px"></object>
+                    <object type="image/svg+xml" data="{{ asset('images/passos/passo_2.svg') }}" style="max-height: 150px"></object>
                     <h6><strong>Selecione o Freelancer</strong></h6>
-                    <p>Lorem ipsum dolor sit amet,
-                        consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor.</p>
+                    <p>Selecione o freelancer com a proposta que te agrada.</p>
                 </div>
                 <div class="col-lg-3 col-xl-3 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <object type="image/svg+xml" data="{{ asset('images/passos/passo_1.svg') }}" style="max-height: 150px"></object>
+                    <object type="image/svg+xml" data="{{ asset('images/passos/passo_3.svg') }}" style="max-height: 150px"></object>
                     <h6><strong>Pague pelo trabalho</strong></h6>
-                    <p>Lorem ipsum dolor sit amet,
-                        consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor.</p>
+                    <p>Pague pelo trabalho e e a plataforma ficará com o valor até o freelancer terminar a execução.</p>
                 </div>
                 <div class="col-lg-3 col-xl-3 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <object type="image/svg+xml" data="{{ asset('images/passos/passo_1.svg') }}" style="max-height: 150px"></object>
+                    <object type="image/svg+xml" data="{{ asset('images/passos/passo_4.svg') }}" style="max-height: 150px"></object>
                     <h6> <strong>Receba o Trabalho</strong></h6>
-                    <p>Lorem ipsum dolor sit amet,
-                        consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor.</p>
+                    <p>Receba o trabalho e a plataforma fará o pagamento ao freelancer e podes avaliar o freelancer.</p>
                 </div>
             </div>
         </div>
