c4e1e3d9f3adebea886698bfbd64a52d35955179
ob1-cuna <anacletocuna@sapo.pt>
2020-09-17 17:36:55 +0200

    actualizador de provincia

---
+++ resources/views/layouts/includes/select_provincias_edit.blade.php
@@ -0,0 +1,10 @@
+<option value="Maputo" @if ($var == 'Maputo') selected @else @endif>Maputo</option>
+<option value="Gaza" @if ($var == 'Gaza') selected @else @endif>Gaza</option>
+<option value="Inhambane" @if ($var == 'Inhambane') selected @else @endif>Inhambane</option>
+<option value="Sofala" @if ($var == 'Sofala') selected @else @endif>Sofala</option>
+<option value="Manica" @if ($var == 'Manica') selected @else @endif>Manica</option>
+<option value="Tete" @if ($var == 'Tete') selected @else @endif>Tete</option>
+<option value="Zambezia" @if ($var == 'Zambezia') selected @else @endif>Zambezia</option>
+<option value="Nampula" @if ($var == 'Nampula') selected @else @endif>Nampula</option>
+<option value="Lichinga" @if ($var == 'Lichinga') selected @else @endif>Lichinga</option>
+<option value="Cabo Delgado" @if ($var == 'Cabo Delgado') selected @else @endif>Cabo Delgado</option>
