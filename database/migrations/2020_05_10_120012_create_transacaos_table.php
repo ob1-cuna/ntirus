1df509ef579b9ac669773793bd99767882fcca97
ob1-cuna <anacletocuna@sapo.pt>
2020-09-30 23:03:22 +0200

    revisão das migrações

--- database/migrations/2020_05_10_120012_create_transacaos_table.php
+++ database/migrations/2020_05_10_120012_create_transacaos_table.php
@@ -15,6 +15,18 @@ class CreateTransacaosTable extends Migration
     {
         Schema::create('transacaos', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('trabalho_id')->unsigned();
+            $table->foreign('trabalho_id')->references('id')->on('trabalhos');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->enum('tipo', ['c2p', 'p2f', 'p2c', 'saque']);
+            $table->double('valor', 8, 2);
+            $table->bigInteger('metodo_id')->unsigned();
+            $table->foreign('metodo_id')->references('id')->on('metodos_de_pagamentos');
+            $table->string('codigover');
+            $table->string('titular');
+            $table->string('destino');
+            $table->enum('estado', ['Pendente', 'Concluido', 'Por Confirmar'])->default('Pendente');
             $table->timestamps();
         });
     }
